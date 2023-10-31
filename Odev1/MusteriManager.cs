21aef9fbacdc67dc350c4fede0f30293d5d9d587
Taha Özen <Taha zen@DESKTOP-3PQUM14>
2023-10-31 17:28:54 +0300

    Add project files.

---
+++ Odev1/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+    internal class MusteriManager
+    {
+        Musteri musteri = new Musteri();
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad + "Müşteri Eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad + "Müşteri Silindi");
+        }
+
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad + musteri.Id);
+        }
+
+    }
+}
---
+++ Odev1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Ad = "Taha";
+            musteri.Soyad = "Özen";
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 2;
+            musteri1.Ad = "İpek";
+            musteri1.Soyad = "Özen";
+
+            Musteri[] musteriler = new Musteri[] { musteri, musteri1 };
+
+            foreach (var Musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " ");
+                Console.WriteLine(musteri.Ad + musteri.Soyad);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri);
+            musteriManager.Sil(musteri1);
+
+        }
+    }
+}
